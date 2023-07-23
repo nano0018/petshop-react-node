e2cf60b7742cd4b5b0cbaceac1a22b85abf107c0
Daniel Cifuentes <dacifuentes@outlook.com>
2023-07-23 17:45:27 -0500

    :construction: Frontend -> Add user manage, customer manage, product manage and category manage pages

--- frontend/src/pages/Admin/ManageOrders/index.jsx
+++ frontend/src/pages/Admin/ManageOrders/index.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import AdminPanelLayout from "@components/AdminPanelLayout";
 import { fetchData } from "@utils/requestHandler";
 import { NavLink } from "react-router-dom";
-import { PencilSquareIcon } from '@heroicons/react/24/solid';
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
 import { GlobalContext } from "@context/GlobalContext";
 
 function ManageOrders() {
@@ -10,6 +10,19 @@ function ManageOrders() {
 
   const [orders, setOrders] = useState();
   const [users, setUsers] = useState();
+
+  const changeDateFormat = (date) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString();
+  };
+
+  const getTotalOrder = (orderedProducts) => {
+    return orderedProducts.reduce(
+      (acc, product) => acc + product.qtyOrderedProduct,
+      0
+    );
+  };
+
   useEffect(() => {
     context?.setActiveSection("orders");
     fetchData("orders/manage").then((data) => setOrders(data));
@@ -19,22 +32,28 @@ function ManageOrders() {
     <AdminPanelLayout>
       <div className="ml-20 md:ml-4 flex flex-col items-center justify-center">
         <table className="text-center">
-          <thead >
+          <thead>
             <tr>
               <th colSpan={3}>Órdenes</th>
             </tr>
             <tr>
               <th>Nombre</th>
-              <th>Stock</th>
+              <th>Fecha</th>
+              <th>Total</th>
             </tr>
           </thead>
           <tbody>
             {orders?.map((order) => (
               <tr key={order._id}>
-                <td>{`${users?.find((user) => user._id === order.userId)?.name}`}</td>
-                <td>{order.stock}</td>
+                <td>{`${
+                  users?.find((user) => user._id === order.userId)?.name
+                }`}</td>
+                <td>{changeDateFormat(order.createdAt)}</td>
+                <td>{getTotalOrder(order.orderedProducts)}</td>
                 <td>
-                <NavLink to={`${order._id}`}><PencilSquareIcon className="h-6 text-gray hover:text-black_blue "/></NavLink>
+                  <NavLink to={`${order._id}`}>
+                    <PencilSquareIcon className="h-6 text-gray hover:text-black_blue " />
+                  </NavLink>
                 </td>
               </tr>
             ))}
